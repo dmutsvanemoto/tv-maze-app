9c59c11af44b474ec9a4ea44c3ed3d28b4635b83
Daniel M <13464314+dmutsvanemoto@users.noreply.github.com>
2021-02-02 10:47:28 +0000

    Added fetchshows api request

--- tv-maze/src/pages/HomePage.tsx
+++ tv-maze/src/pages/HomePage.tsx
@@ -1,4 +1,7 @@
-import {FC} from 'react';
+import {FC, useState, useEffect} from 'react';
+import axios from 'axios';
+import { Show } from '../../../interfaces/Show';
+import { fetchShows } from '../api/fetchShows';
 import {HomeBanner} from '../components/HomeBanner/HomeBanner';
 import {LatestShows} from '../components/LatestShows/LatestShows';
 
@@ -12,12 +15,34 @@ const HomePageData = {
 }
 
 export const HomePage: FC<HomePageProps> = () => {
+    const [shows, setShows] = useState<Show[]>([]);
+    const [showError, setShowError] = useState(false);
+
+    useEffect(() => {
+        const loadShows = async () => {
+            const CancelToken = axios.CancelToken;
+            const source = CancelToken.source();
+    
+            var {isError, data} = await fetchShows(source);
+
+            if(isError)
+            {
+                setShowError(true);
+            } else {
+                setShows(data);
+            }    
+        }
+
+        loadShows();
+
+        return () => {
+            setShowError(false)
+        }
+    }, [])
+    
+
     return <>
         <HomeBanner title={HomePageData.title} description={HomePageData.description} />
-        <LatestShows shows={[]} />
+        <LatestShows shows={shows} />
     </>
 }
-
-
-
-
